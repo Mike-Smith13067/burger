105fa986feb8c138ca6fd5f27aa965e57df6b35f
Mike Smith <hockeydad66@live.com>
2020-01-12 14:58:18 -0700

    created orm.js

---
+++ config/orm.js
@@ -0,0 +1,25 @@
+var connection = require("../config/connection");
+
+var orm = {
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM ?";
+        connection.query(queryString, [table], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function(table, colName, value, cb) {
+        var queryString = "INSERT INTO ?? (??) VALUES (?)";
+        connection.query(queryString [table, colName, value], function (err, result) {
+            if(err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function(table, updateCol, updateVal, identifyingCol, identifyingVal, cb) {
+        var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        connection.query(queryString, [table, updateCol, updateVal, identifyingCol, IdentifyingVal], function(err, result) {
+            if(err) throw err;
+            cb(result);
+        });
+    }
+};
